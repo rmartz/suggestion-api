02a9f0487dc85d8431cc6642c03afeba8700db33
Reed Martz <reed@reedmartz.com>
2019-12-19 16:26:34 -0500

    Ignore unused imports in all __init__.py files

--- app/voting/serializers/__init__.py
+++ app/voting/serializers/__init__.py
@@ -1,4 +1,4 @@
-from .ballot import BallotSerializer  # noqa: F401
-from .ballot_option import BallotOptionSerializer  # noqa: F401
-from .room import RoomSerializer  # noqa: F401
-from .voting_session import VotingSessionSerializer  # noqa: F401
+from .ballot import BallotSerializer
+from .ballot_option import BallotOptionSerializer
+from .room import RoomSerializer
+from .voting_session import VotingSessionSerializer
--- app/voting/views/__init__.py
+++ app/voting/views/__init__.py
@@ -1,4 +1,4 @@
-from .ballot import BallotViewSet  # noqa: F401
-from .ballot_option import BallotOptionViewSet  # noqa: F401
-from .room import RoomViewSet  # noqa: F401
-from .voting_session import VotingSessionViewSet  # noqa: F401
+from .ballot import BallotViewSet
+from .ballot_option import BallotOptionViewSet
+from .room import RoomViewSet
+from .voting_session import VotingSessionViewSet
